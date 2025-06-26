124e0fa22cfc6e5f175a98a638ab9abbfe9d6c5b
codermuskan-hub <iammuskan796@gmail.com>
2025-06-26 17:13:12 +0530

    static arrays

---
+++ basic.py
@@ -0,0 +1,7 @@
+#this includes dsa basics
+#first lets tackle with
+#STATIC ARRAYS
+#we hear static all the time, it simply means -unchangable 
+#these arrays are contiguously stored in the memory 
+#Analogy: think of it as the number of shelves in a book shelf you built yourself. You got to decide how many shelves the shelf would have while you are making it but once it is made you cannot change the number of shelves.
+
